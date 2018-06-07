3723c8e169875f9083270144552756adc25e50c8
greenfrog <greenfrog82@naver.com>
2018-06-07 20:01:54 +0900

    How to write additional color information.

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,22 +1,18 @@
 <template>
   <div id="app">
     <v-app light>
-      <v-toolabr class="white">
+      <v-toolbar class="white">
         <v-toolbar-title v-text=title></v-toolbar-title>
-      </v-toolabr>
+      </v-toolbar>
       <v-content>
         <router-view/>
       </v-content>
       <v-footer height="auto">
       <v-card flat tile class="flex">
-        <v-card-title class="teal white--text">
-          <strong class="subheading">Get connected with us on social networks!</strong>
-          <v-spacer></v-spacer>
-          <v-btn
-            icon
-            dark
-            class="mx-3"
-          >
+        <!-- https://vuetifyjs.com/en/style/colors -->
+        <v-card-title class="black grey--text text--lighten-1">
+          <strong class="subheading">Let's contribute our code!</strong>
+          <v-btn icon dark class="mx-3">
             <v-icon size="24px">fab fa-github</v-icon>
           </v-btn>
         </v-card-title>

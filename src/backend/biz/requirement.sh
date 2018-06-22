511bdfcc29726f56d3674a3d808c9e7bf1e28ece
Seungho Ryu <seungho.ryu@cdnetworks.co.kr>
2018-06-23 01:25:16 +0900

    Update backend env

--- src/backend/biz/requirement.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-go get github.com/gin-gonic/gin
-# gin - https://github.com/codegangsta/gin, This is for hot re-loading go webserver
-go get github.com/codegangsta/gin
-go get github.com/jinzhu/gorm
-go get github.com/jinzhu/gorm/dialects/postgres
-go get github.com/dgrijalva/jwt-go
